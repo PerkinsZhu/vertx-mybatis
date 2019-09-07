79fe4bbfb3de821aea7dbcb2100fa46f49ebf7f9
PerkinsZhu <perkinszhu@sinia.cn>
2019-09-07 20:08:45 +0800

    实现数据源的动态绑定。支持注解和配置文件方式

--- src/main/java/com/perkins/mapper0/UserMapper0.java
+++ src/main/java/com/perkins/mapper0/UserMapper0.java
@@ -1,4 +1,4 @@
-package com.perkins.mapper;
+package com.perkins.mapper0;
 
 import com.perkins.entity.User;
 import org.apache.ibatis.annotations.Mapper;
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper0 {
 
     @Select("select * from user")
     public List<User> listUser();
